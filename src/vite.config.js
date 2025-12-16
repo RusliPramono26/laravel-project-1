f0f2c5dceeec97bdc681ccfd3db2fdc3acbb5685
Rusli Pramono <92093865+Rusli-5200411417@users.noreply.github.com>
2025-12-16 19:31:18 +0700

    setting:finale setting vue

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: '127.0.0.1',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
